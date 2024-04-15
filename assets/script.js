0ecded6fad452979c2153f4c6c694ce2d5cd2bdd
Vitor Pires <tor_pires@hotmail.com>
2024-04-15 06:42:17 -0300

    Adicionado o script.js

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const peso = document.getElementById('weight').value
+    const altura = document.getElementById('height').value
+
+    const imc = (peso/(altura * altura)).toFixed(2)
+
+    const valor = document.getElementById('value')
+    let descricao = ""
+
+    value.classList.add('attention')
+    document.getElementById('infos').classList.remove('hidden')
+
+
+    if (imc < 18.5){
+        descricao = "Cuidado! Você está abaixo do peso! "
+    }
+    else if (imc >= 18.5 && imc <= 25){
+        descricao = "Você está no peso ideal!"
+        value.classList.remove('attention')
+        value.classList.add('normal')
+    } 
+    else if (imc >25.0 && imc <= 30){
+        descricao = "Cuidado! Você está acima do peso!"
+    } 
+    else if (imc > 30 && imc <= 35){
+        descricao = "Cuidado! Você está com obesidade moderada!"
+    } 
+    else if (imc > 35 && imc <= 40){
+        descricao = "Cuidado! Você está com obesidade severa!"
+    }
+    else{
+        descricao = "Cuidado! Você está com obesidade morbida!"
+
+    } 
+
+    value.textContent = imc.replace('.', ",")
+    document.getElementById('description').textContent = descricao
+})
